ddd5ea69ca843ae7986c334343a6780c4da706c8
emsi-g <marcchristianp.gregorio@gmail.com>
2024-06-08 13:16:49 +0800

    Change AWS Account ID

--- scripts/start-process.sh
+++ scripts/start-process.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo -u root aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 083341655278.dkr.ecr.ap-southeast-1.amazonaws.com
+sudo -u root aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin xxxxxxxx.dkr.ecr.ap-southeast-1.amazonaws.com
 sudo -u root docker rm -f `docker ps -q -a`
 sudo -u root docker rmi -f `docker images -q`
 sleep 3
-sudo -u root docker pull 083341655278.dkr.ecr.ap-southeast-1.amazonaws.com/aws-codepipeline-project:latest
+sudo -u root docker pull xxxxxxxx.dkr.ecr.ap-southeast-1.amazonaws.com/aws-codepipeline-project:latest
 if [[ $? != 0 ]]
 then
-    sudo -u root aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 083341655278.dkr.ecr.ap-southeast-1.amazonaws.com
-    sudo -u root docker pull 083341655278.dkr.ecr.ap-southeast-1.amazonaws.com/aws-codepipeline-project:latest
+    sudo -u root aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin xxxxxxxx.dkr.ecr.ap-southeast-1.amazonaws.com
+    sudo -u root docker pull xxxxxxxx.dkr.ecr.ap-southeast-1.amazonaws.com/aws-codepipeline-project:latest
 fi
-sudo -u root docker run -d -p 80:3000 --name aws-codepipeline-project 083341655278.dkr.ecr.ap-southeast-1.amazonaws.com/aws-codepipeline-project:latest
+sudo -u root docker run -d -p 80:3000 --name aws-codepipeline-project xxxxxxxx.dkr.ecr.ap-southeast-1.amazonaws.com/aws-codepipeline-project:latest
